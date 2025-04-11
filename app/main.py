b44a35c2c2cb4f21ed12c43f2be39edcb4cd3825
WilliamCuc <wcuc96@gmail.com>
2025-04-10 19:00:20 -0600

    Nuevas librerias para instalar

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from app.routes.user_routes import router as user_router
 
@@ -7,4 +8,5 @@ app.include_router(user_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Usa el puerto de la variable de entorno o 8000 por defecto
+    uvicorn.run(app, host="0.0.0.0", port=port)
